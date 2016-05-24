b8b1c6f6311b964b98c69b175a716cfb33f6bcce
Dragon-L <530593201@qq.com>
2016-05-24 17:17:32 +0800

    refactor

--- src/main.js
+++ src/main.js
@@ -1,23 +1,18 @@
 function printInventory(inputs){
-    var goodsArray = [];
-    var subtotal = 0;
-    for(var i = 0,len = inputs.length;i < len;i++){
-        insertGoods(inputs[i],goodsArray);
-    }
-    var out = "***<没钱赚商店>购物清单***\n";
-    for(i in goodsArray){
-        goodsArray[i].subtotal = goodsArray[i].count * goodsArray[i].price;
-        out += "名称：" + goodsArray[i].name +"，数量："+goodsArray[i].count+
-            goodsArray[i].unit+"，单价："+goodsArray[i].price.toFixed(2)+"(元)，小计："+
-            goodsArray[i].subtotal.toFixed(2)+"(元)\n";
-    }
-    for(i in goodsArray){
-        subtotal += goodsArray[i].subtotal;
-    }
-    out += '----------------------\n'+'总计：'+subtotal.toFixed(2)+'(元)\n'+'**********************';
-    console.log(out);
+    var cartItems = [];
+    var total = 0;
+    cartItems = jasonToCartItems(inputs);
+    total = adjustCartItems(cartItems);
+    printCartItems(cartItems,total);
 }
-function insertGoods(input,array){
+function jasonToCartItems(jason) {
+    var cartItems = [];
+    for(var i = 0,len = jason.length;i < len;i++){
+        pushcartItems(jason[i],cartItems);
+    }
+    return cartItems;
+}
+function pushcartItems(input,array){
     if (array[input.barcode]){
         array[input.barcode].count++;
     } else {
@@ -28,4 +23,30 @@ function insertGoods(input,array){
         array[input.barcode].price = input.price;
         array[input.barcode].barcode = input.barcode;
     }
+}
+function adjustCartItems(cartItems) {
+    for(i in cartItems){
+        cartItems[i].price = cartItems[i].price;
+        cartItems[i].subtotal = (cartItems[i].count * cartItems[i].price);
+    }
+    var total = 0.00;
+    for(i in cartItems){
+        total += cartItems[i].subtotal;
+    }
+    return total;
+}
+function printCartItems(cartItems,total) {
+    var out = "***<没钱赚商店>购物清单***\n";
+    for(i in cartItems){
+        out += "名称：" + cartItems[i].name +
+            "，数量："+ cartItems[i].count + cartItems[i].unit +
+            "，单价：" + cartItems[i].price.toFixed(2) +
+            "(元)，小计：" + cartItems[i].subtotal.toFixed(2) +
+            "(元)\n";
+    }
+
+    out += '----------------------\n' +
+        '总计：'+total.toFixed(2) + '(元)\n' +
+        '**********************';
+    console.log(out);
 }

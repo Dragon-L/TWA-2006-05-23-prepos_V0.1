128a510956b38d34521685215b26c0fb4eeab389
Dragon-L <530593201@qq.com>
2016-05-24 16:29:50 +0800

    change

--- src/main.js
+++ src/main.js
@@ -1,27 +1,29 @@
 function printInventory(inputs){
-    var goodsArray = new Array(),total = 0;
-    for (var i = 0;i<inputs.length;i++){
+    var goodsArray = [];
+    var subtotal = 0;
+    for(var i = 0,len = inputs.length;i < len;i++){
         insertGoods(inputs[i],goodsArray);
     }
     var out = "***<没钱赚商店>购物清单***\n";
-    for(x in goodsArray){
-        var totalPrice = goodsArray[x].number*goodsArray[x].price;
-        out += "名称：" + goodsArray[x].name +"，数量："+goodsArray[x].number+
-            goodsArray[x].unit+"，单价："+goodsArray[x].price.toFixed(2)+"(元)，小计："+
-            totalPrice.toFixed(2)+"(元)\n";
-        total = total + totalPrice;
+    for(i in goodsArray){
+        goodsArray[i].subtotal = goodsArray[i].count * goodsArray[i].price;
+        out += "名称：" + goodsArray[i].name +"，数量："+goodsArray[i].count+
+            goodsArray[i].unit+"，单价："+goodsArray[i].price.toFixed(2)+"(元)，小计："+
+            goodsArray[i].subtotal.toFixed(2)+"(元)\n";
     }
-    out += '----------------------\n'+'总计：'+total.toFixed(2)+'(元)\n'+'**********************';
+    for(i in goodsArray){
+        subtotal += goodsArray[i].subtotal;
+    }
+    out += '----------------------\n'+'总计：'+subtotal.toFixed(2)+'(元)\n'+'**********************';
     console.log(out);
 }
 function insertGoods(input,array){
-   if (array[input.barcode]){
-        array[input.barcode].number++;
-    }
-    else {
-        array[input.barcode] = new Object();
+    if (array[input.barcode]){
+        array[input.barcode].count++;
+    } else {
+        array[input.barcode] = {};
         array[input.barcode].name = input.name;
-        array[input.barcode].number = 1;
+        array[input.barcode].count = 1;
         array[input.barcode].unit = input.unit;
         array[input.barcode].price = input.price;
         array[input.barcode].barcode = input.barcode;
